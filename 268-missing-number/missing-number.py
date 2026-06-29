2a8499bc1c1c1bb73bba431f4fa4dbc3c845510d
vanshsoni4 <vanshsoni104@gmail.com>
2026-06-30 04:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (67.29%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        n = len(nums)
+        expectedsum = n*(n+1)//2
+        actualsum = sum(nums)
+        return expectedsum - actualsum
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
