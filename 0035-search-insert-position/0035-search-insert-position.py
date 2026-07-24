7f5bd3263642e93065cdb00068c252fdf5127827
vanshsoni4 <vanshsoni104@gmail.com>
2026-07-25 00:29:44 +0530

    Time: 0 ms (100%), Space: 13.1 MB (29.31%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i]>=target:
+                return i
+        return len(nums)
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
