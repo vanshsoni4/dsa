3d2ff0e560e39671284daf199c7fe3c2f02b63ca
vanshsoni4 <vanshsoni104@gmail.com>
2026-07-25 01:09:25 +0530

    Time: 0 ms (100%), Space: 13.1 MB (72.17%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,13 +1,39 @@
 class Solution(object):
+    def binarysearchleft(self,nums,target):
+        n =len(nums)
+        low,high =0,n-1
+        index=-1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid]==target:
+                index=mid
+                high = mid-1
+            elif nums[mid]>target:
+                high =  mid-1
+            else:
+                low=mid+1
+        return index
+    def binarysearchright(self,nums,target):
+        n = len(nums)
+        low,high=0,n-1
+        index=-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                index=mid
+                low = mid+1
+            elif nums[mid]>target:
+                high = mid-1
+            else:
+                low =mid+1
+        return index
     def searchRange(self, nums, target):
-        first = -1
-        last = -1
-        for i in range(len(nums)):
-            if nums[i]==target:
-                if first==-1:
-                    first = i
-                last = i
-        return [first,last]
+        extleft = self.binarysearchleft(nums,target)
+        if extleft==-1:
+            return [-1,-1]
+        extright = self.binarysearchright(nums,target)
+        return[extleft,extright]
+        
         """
         :type nums: List[int]
         :type target: int
